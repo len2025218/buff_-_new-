72bd2d0ebf1eefe668b045a0e75800b145dd5a93
len2025218 <2864211629@qq.com>
2026-03-05 16:34:05 +0800

    Add files via upload

---
+++ CMakeFiles/armor.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/Buff/buff_detector/Buff_Detector.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/Buff/buff_detector/Buff_Detector.cpp.o"
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/Buff/buff_detector/inference_api2.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/Buff/buff_detector/inference_api2.cpp.o"
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/Buff/buff_processor/Buff_Coordsolver.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/Buff/buff_processor/Buff_Coordsolver.cpp.o"
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/Buff/buff_processor/Buff_Processor.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/Buff/buff_processor/Buff_Processor.cpp.o"
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/Buff/buff_processor/filter/buff_predictor.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/Buff/buff_processor/filter/buff_predictor.cpp.o"
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/Buff/buff_processor/filter/linear_particle_filter.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/Buff/buff_processor/filter/linear_particle_filter.cpp.o"
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/camera/MindVisionCamera.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/camera/MindVisionCamera.cpp.o"
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/main.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/main.cpp.o"
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/serial/CRC_Check.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/serial/CRC_Check.cpp.o"
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/serial/data_transform.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/serial/data_transform.cpp.o"
+  "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/serial/serialport.cpp" "/home/echo/BBBBB/buff_多线程 new/buff_多线程 (1)/buff_多线程/CMakeFiles/armor.dir/serial/serialport.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CERES_EXPORT_INTERNAL_SYMBOLS"
+  "GFLAGS_IS_A_DLL=0"
+  "TBB_PREVIEW_WAITING_FOR_WORKERS=1"
+  "_FILE_OFFSET_BITS=64"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  "/usr/local/include/eigen3"
+  "/opt/l_openvino_toolkit_ubuntu20_2022.2.0.7713.af16ea1d79a_x86_64/runtime/include"
+  "/opt/l_openvino_toolkit_ubuntu20_2022.2.0.7713.af16ea1d79a_x86_64/runtime/include/ie"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/armor.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/armor.dir/Buff/buff_detector/Buff_Detector.cpp.o"
+  "CMakeFiles/armor.dir/Buff/buff_detector/inference_api2.cpp.o"
+  "CMakeFiles/armor.dir/Buff/buff_processor/Buff_Coordsolver.cpp.o"
+  "CMakeFiles/armor.dir/Buff/buff_processor/Buff_Processor.cpp.o"
+  "CMakeFiles/armor.dir/Buff/buff_processor/filter/buff_predictor.cpp.o"
+  "CMakeFiles/armor.dir/Buff/buff_processor/filter/linear_particle_filter.cpp.o"
+  "CMakeFiles/armor.dir/camera/MindVisionCamera.cpp.o"
+  "CMakeFiles/armor.dir/main.cpp.o"
+  "CMakeFiles/armor.dir/serial/CRC_Check.cpp.o"
+  "CMakeFiles/armor.dir/serial/data_transform.cpp.o"
+  "CMakeFiles/armor.dir/serial/serialport.cpp.o"
+  "armor"
+  "armor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/armor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
